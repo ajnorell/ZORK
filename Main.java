fd847c44be4d21dbabfb0c1698d9171072c4c198
ajnorell <54289182+ajnorell@users.noreply.github.com>
2020-02-12 14:25:13 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+ * Main Class - the runner class of the "Zork" game.
+ *
+ * Author:  April Schermann
+ * Version: 1
+ * Date:    October 2018
+ * 
+ *  To play this game, the main method creates an instance 
+ *  of the Game class and calls the "play" method.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.play();
+	}
+}

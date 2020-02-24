efab1350f991fa1ff06bbda156b66ca14114871a
elliedance <54289910+elliedance@users.noreply.github.com>
2020-02-24 13:56:45 -0600

    Update CommandWords.java

--- CommandWords.java
+++ CommandWords.java
@@ -13,7 +13,7 @@ class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String validCommands[] = {
-        "go", "quit", "help", "fly"
+        "w", "a", "s", "d"
     };
 
     /**

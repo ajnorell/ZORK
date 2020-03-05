705d0f3fd5c6f126d0e618086f047d4b327926e1
ajnorell <54289182+ajnorell@users.noreply.github.com>
2020-03-05 11:26:32 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -158,6 +158,12 @@ class Game
         else if(commandWord.equals("d")){
             zorkArray.move(0, 1);
         }
+        else if(commandWord.equals("open"){
+            if((map[48][37] == 2) || (map[48][39] == 2))
+                System.out.println("5s26W$$nC*hpa,E");
+            else
+                System.out.println('That is not a command I recognize.");
+        }
         else if(commandWord.equals("stats")){
             System.out.println("Health: "+character.hp);
             System.out.println("Sp: "+character.sp);
